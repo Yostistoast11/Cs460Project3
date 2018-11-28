7b0e2fb848e65e4d49bad68204178f1ed073f63e
Caleb C Yost <cyost@blue.cs.sonoma.edu>
2018-11-28 15:44:19 -0800

    First commit

---
+++ SyntacticalAnalyzer.h
@@ -0,0 +1,42 @@
+#ifndef SYN_H
+#define SYN_H
+
+#include <iostream>
+#include <fstream>
+#include "LexicalAnalyzer.h"
+
+using namespace std;
+
+class SyntacticalAnalyzer 
+{
+    public:
+	SyntacticalAnalyzer (char * filename);
+	~SyntacticalAnalyzer ();
+	int program();
+        int more_defines();
+        int define();
+        int stmt_list();
+        int stmt();
+        int literal();
+        int more_tokens();
+        int quoted_lit();
+        int param_list();
+        int else_part();
+        int stmt_pair();
+        int stmt_pair_body();
+        int action();
+        int any_other_token();
+	void ReportError (const string & msg);
+    private:
+	int errors;
+	LexicalAnalyzer * lex;
+	token_type token;
+	ifstream input;
+	ofstream listingFile;
+	ofstream ruleFile;
+	ofstream debugFile;
+	string currentLine;
+
+};
+	
+#endif
